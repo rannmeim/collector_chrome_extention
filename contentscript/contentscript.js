8c7b633600ff5d3c3f9a79d46158f888a8a280c7
rmm <mmei_ran@163.com>
2020-10-20 17:44:14 +0800

    feat: show function box on double click

--- contentscript/contentscript.js
+++ contentscript/contentscript.js
@@ -140,23 +140,34 @@ function handleAddBtnClick() {
     highlightSelection();
 }
 
-document.addEventListener('mouseup', function (e) {
-    let selection = document.getSelection();
-    if (mouseMoved && selection.toString()) {
-        geneFunctionBox(e, selection);
-    }
-}, false);
-
 document.addEventListener('click', function (e) {
+    console.log('click')
     if (!mouseMoved) {
         disposeFunctionBox();
     }
     mouseMoved = false;
 });
+document.addEventListener('mouseup', function (e) {
+    let selection = document.getSelection();
+    console.log('mouse up', selection.toString())
+    if (selection.toString()) {
+        console.log('about to render:', selection.toString())
+        geneFunctionBox(e, selection);
+    }
+}, false);
 
 document.addEventListener('mousedown', function (e) {
     mouseMoved = false;
 }, false);
 document.addEventListener('mousemove', function (e) {
     mouseMoved = true;
+}, false);
+document.addEventListener('dblclick', function (e) {
+    console.log('double click')
+    let selection = document.getSelection();
+    if (selection.toString()) {
+        // console.log('about to render:', selection.toString())
+        geneFunctionBox(e, selection);
+    }
+
 }, false);
